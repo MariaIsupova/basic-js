4211074b5e75df9bc6a0b4146e33c50ca1b465d7
MariaIsupova <Mariaisisupova@gmail.com>
2020-10-02 21:52:31 +0300

    solution count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  var count = 0
+  matrix.forEach(row => {
+    row.forEach(element => {
+      if (element === '^^') {
+        count += 1
+      }
+    });
+  });
+  return count
 };
