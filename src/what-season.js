33e67f510d6ece20aac732153f9e2167f20f0e6a
MariaIsupova <Mariaisisupova@gmail.com>
2020-10-31 02:19:03 +0300

    feat: add transform-array

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(checkDate) {
+  console.log('input', checkDate)
+  if (checkDate === undefined) {
+    console.log('no')
+    return 'Unable to determine the time of year!'
+    // throw new CustomError('Not implemented');
+  }
+  if (typeof checkDate.getMonth === 'function') {
+    console.log('+')
+    var month = checkDate.getMonth()
+    console.log(month)
+    if (month === 0 || month === 1 || month === 11) {
+      return 'winter'
+    } else if (month === 2 || month === 3 || month === 4) {
+      return 'spring'
+    } else if (month === 5 || month === 6 || month === 7) {
+      return 'summer'
+    } else {
+      return 'autumn'
+    }
+  } else {
+    console.log('-')
+    throw new CustomError('Not implemented');
+  }
+
 };
